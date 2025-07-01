81b5bf97816f26778ab9b0430b72b0f9a123a97c
Praveen Tiwari <praveentiwari29004@gmail.com>
2025-07-02 01:42:49 +0530

    div 3

---
+++ B_Tournament.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+void Solve(){
+    int n,j,k;
+    cin>>n>>j>>k;
+    vector<int> v(n);
+    cin>>v;
+    if(k>n){
+        cout<<"NO"<<endl;
+        return;
+    }   
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
+    for(auto it: v){
+        mini = min(mini,it);
+        maxi = max(maxi,it);
+    }
+    if(k==1){
+        if(v[j-1]==maxi){
+            cout<<"YES"<<endl;
+            return;
+        }
+        cout<<"NO"<<endl;
+        return;
+    }
+    cout<<"YES"<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases = 1;
+    cin >> test_cases;
+    for(int tc = 1; tc <= test_cases; tc++){
+        // cout << "Case #" << tc << ": ";k
+        Solve();
+    }
+    return 0;
+}
